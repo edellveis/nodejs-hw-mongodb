0b16dcbeae1daff2aabfd6221bd5427ca9e9f6dd
edellveis <161501144+edellveis@users.noreply.github.com>
2024-12-01 22:49:02 +0200

    hw-nodejs

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,12 +1,10 @@
-import { PATH_DB } from '../constants/contacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
-import * as fs from 'node:fs/promises';
 
-const generateContacts = async (number) => {
-  const contactsList = Array(number).fill(null).map(createFakeContact);
-  fs.readFile(PATH_DB, (error, data) => {
-    console.log(error);
-    console.log(data);
-  });
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const generateContacts = async (number) => {
+  const contactsList = Array(number).fill(0).map(createFakeContact);
+
+  writeContacts(contactsList);
 };
 generateContacts(5);
