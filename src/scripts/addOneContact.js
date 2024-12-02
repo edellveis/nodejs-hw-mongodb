daba4832400a2cd1cf1b3dbfa6ecee0161088e4a
edellveis <161501144+edellveis@users.noreply.github.com>
2024-12-02 15:33:53 +0200

    hw-fix

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,4 +1,6 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
 export const addOneContact = async () => {
   const contactList = await readContacts();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,12 +1,9 @@
-import { PATH_DB } from '../constants/contacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
-import * as fs from 'node:fs/promises';
+import { writeContacts } from '../utils/writeContacts.js';
 
-const generateContacts = async (number) => {
-  const contactsList = Array(number).fill(null).map(createFakeContact);
-  fs.readFile(PATH_DB, (error, data) => {
-    console.log(error);
-    console.log(data);
-  });
+export const generateContacts = async (number) => {
+  const contactsList = Array(number).fill(0).map(createFakeContact);
+
+  writeContacts(contactsList);
 };
 generateContacts(5);
